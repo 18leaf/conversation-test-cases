2a8f210b42de7eca7904ffe2c11849ab3323c774
Lindsey <18leaf@gmail.com>
2025-02-09 19:36:15 -0500

    add .gitignore and update directory for ipc_demo

--- tests/test_ipc.cpp
+++ tests/test_ipc.cpp
@@ -3,8 +3,7 @@
 
 int main() {
 	std::cout << "Test IPC: Running IPC demo." << std::endl;
-	// Assumes that the ipc_demo executable is built in bin/.
-	system("./bin/ipc_demo");
+	system("./ipc_demo");
 	std::cout << "Test IPC: Completed." << std::endl;
 	return 0;
 }
